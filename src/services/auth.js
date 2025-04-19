a5e3d5e859f0fa3e80acfc9a6d61fb73fdbc835f
olivia-dp <olivia.osypovych@gmail.com>
2025-04-19 19:52:34 +0300

    current user

--- src/services/auth.js
+++ src/services/auth.js
@@ -5,14 +5,32 @@ import SessionCollection from "../models/sessionSchema.js";
 
 
 export const registerUser = async (payload) => {
-    const user = await UserCollection.findOne({ email: payload.email });
-  if (user) throw createHttpError(409, 'Email in use');
+    const existingUser = await UserCollection.findOne({ email: payload.email });
+  if (existingUser) throw createHttpError(409, 'Email in use');
 
   const encryptedPassword = await bcrypt.hash(payload.password, 10);
-  return await UserCollection.create({
-    ...payload,
-    password: encryptedPassword,
-  });
+
+const user = await UserCollection.create({
+  ...payload,
+  password: encryptedPassword,
+});
+
+const accessToken = randomBytes(30).toString('base64');
+    const refreshToken = randomBytes(30).toString('base64');
+
+    const session = await SessionCollection.create({
+      userId: user._id,
+      accessToken,
+      refreshToken,
+      accessTokenValidUntil: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      refreshTokenValidUntil: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    });
+
+
+  return {
+    user,
+    accessToken: session.accessToken,
+};
 };
 
 export const loginUser = async (payload) => {
