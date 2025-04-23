b7662334968430c4857c352ca027f3668e58831c
olivia-dp <olivia.osypovych@gmail.com>
2025-04-23 11:49:21 +0300

    cors

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ export const setupServer = async () => {
 
     app.use(
       cors({
-        origin: 'http://localhost:5174',
+        origin: 'http://localhost:5173',
         credentials: true,
       }),
     );
