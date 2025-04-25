d56574f04c0c2239fd7f95d5e7acfb9952ae6d4a
olivia-dp <olivia.osypovych@gmail.com>
2025-04-25 14:22:43 +0300

    fix

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,39 +6,31 @@ import createHttpError from 'http-errors';
 
 
 export const registerUser = async (payload) => {
-    const existingUser = await UserCollection.findOne({ email: payload.email });
-  if (existingUser) throw createHttpError(409, 'Email in use');
+  const existingUser = await UserCollection.findOne({ email: payload.email });
+if (existingUser) throw createHttpError(409, 'Email in use');
 
-  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+const encryptedPassword = await bcrypt.hash(payload.password, 10);
 
-  try {
-    const user = await UserCollection.create({
-      ...payload,
-      email: normalizedEmail,
-      password: encryptedPassword,
-    });
-  
-    const accessToken = randomBytes(30).toString('base64');
-    const refreshToken = randomBytes(30).toString('base64');
+const user = await UserCollection.create({
+...payload,
+password: encryptedPassword,
+});
 
-    const session = await SessionCollection.create({
-      userId: user._id,
-      accessToken,
-      refreshToken,
-      accessTokenValidUntil: new Date(Date.now() + 24 * 60 * 60 * 1000),
-      refreshTokenValidUntil: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-    });
+const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
 
-  } catch (error) {
-    if (error.code === 11000) {
-      throw createHttpError(409, 'Email already exists');
-    }
-    throw createHttpError(500, 'Server error');
-  }
+  const session = await SessionCollection.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    refreshTokenValidUntil: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+  });
 
-  return {
-    user,
-    session,
+
+return {
+  user,
+  session,
 };
 };
 
