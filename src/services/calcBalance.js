919bbe5902c04f5ffcfc1ab2429b4371a4c750e7
PKdev <pasha150898@gmail.com>
2025-04-29 19:18:09 +0200

    balance

---
+++ src/services/calcBalance.js
@@ -0,0 +1,15 @@
+import { TransactionsCollection } from '../models/transaction.js';
+import UserCollection from '../models/userSchema.js';
+
+export const recalculateUserBalance = async (userId) => {
+  const transactions = await TransactionsCollection.find({ userId });
+  const newBalance = transactions.reduce((acc, tx) => {
+    if (tx.type === '+') return acc + tx.sum;
+    if (tx.type === '-') return acc - tx.sum;
+    return acc;
+  }, 0);
+
+  await UserCollection.findByIdAndUpdate(userId, { balance: newBalance });
+
+  return newBalance;
+};
