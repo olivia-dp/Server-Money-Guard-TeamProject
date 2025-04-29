cffb07b44f7a379d85bf81da827dd0b65104057f
PKdev <pasha150898@gmail.com>
2025-04-29 23:18:13 +0200

    changed types to income and expense

--- src/services/calcBalance.js
+++ src/services/calcBalance.js
@@ -4,8 +4,8 @@ import UserCollection from '../models/userSchema.js';
 export const recalculateUserBalance = async (userId) => {
   const transactions = await TransactionsCollection.find({ userId });
   const newBalance = transactions.reduce((acc, tx) => {
-    if (tx.type === '+') return acc + tx.sum;
-    if (tx.type === '-') return acc - tx.sum;
+    if (tx.type === 'Income') return acc + tx.sum;
+    if (tx.type === 'Expense') return acc - tx.sum;
     return acc;
   }, 0);
 
