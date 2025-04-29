1ef4c3f4d89b4a7b4638b7ad2c59e3581845360d
PKdev <pasha150898@gmail.com>
2025-04-30 00:32:36 +0200

    Update calcBalance.js

--- src/services/calcBalance.js
+++ src/services/calcBalance.js
@@ -4,8 +4,8 @@ import UserCollection from '../models/userSchema.js';
 export const recalculateUserBalance = async (userId) => {
   const transactions = await TransactionsCollection.find({ userId });
   const newBalance = transactions.reduce((acc, tx) => {
-    if (tx.type === 'Income') return acc + tx.sum;
-    if (tx.type === 'Expense') return acc - tx.sum;
+    if (tx.type === 'INCOME') return acc + tx.sum;
+    if (tx.type === 'EXPENSE') return acc - tx.sum;
     return acc;
   }, 0);
 
